01bfe4ff59980db28c4a5f6d3b3a9d529b292448
kajj8808 <a01039388808@gmail.com>
2023-07-16 21:16:47 +0900

    파일 업로드 기능 추가 + 스타일 수정

--- SocketFront/pages/index.tsx
+++ SocketFront/pages/index.tsx
@@ -1,8 +1,6 @@
 import type { NextPage } from "next";
 import { socket } from "@libs/socket";
-import { useEffect, useRef, useState } from "react";
-import Link from "next/link";
-import Image from "next/image";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 import { cls } from "@libs/utiles";
 import { useForm } from "react-hook-form";
 import Message from "@components/Message";
@@ -21,6 +19,7 @@ const Home: NextPage = () => {
   const [nickName, setNickName] = useState(new Date().getTime());
   const [openRooms, setOpenRooms] = useState<number[]>([]);
   const [roomId, setRoomId] = useState<number | null>();
+  const [showMediaBox, setShowMediaBox] = useState(false);
   const [messages, setMessages] = useState<IMessage[]>([]);
   const [avatar, setAvatar] = useState(" avatar url... ");
   const { register, handleSubmit, reset } = useForm<EnterForm>();
@@ -43,6 +42,26 @@ const Home: NextPage = () => {
 
   const joinRoom = (roomName: number) => {
     socket.emit("join_room", roomName, () => setRoomId(roomName));
+  };
+
+  const goBackHome = () => {
+    setRoomId(null);
+  };
+
+  const toggleShowMediaBox = () => {
+    setShowMediaBox((prev) => !prev);
+  };
+
+  const handleChangeFile = (input: ChangeEvent<HTMLInputElement>) => {
+    if (input.target.files) {
+      const payload = JSON.stringify({
+        data: input.target.files[0].name,
+        avatar,
+        nickName,
+      });
+      socket.emit("new_message", payload, roomId);
+      toggleShowMediaBox();
+    }
   };
 
   useEffect(() => {
@@ -80,12 +99,20 @@ const Home: NextPage = () => {
   }, [messages.length]);
 
   return (
-    <div className="flex w-full">
+    <div className="flex w-full select-none">
       {roomId ? (
         <div className="w-full">
-          <div className="fixed top-0 z-10 w-full bg-[#F7F9FC] py-6 px-6 backdrop-blur-lg">
-            <span className="text-xl">🏠{roomId}</span>
-            <span></span>
+          <div className="fixed top-0 z-10 flex w-full items-center justify-between bg-[#F7F9FC] py-4 px-4 backdrop-blur-lg">
+            <div className="relative flex h-8 w-8 items-center justify-center rounded-full text-lg font-bold hover:bg-[#aebbd4]">
+              <span
+                className="absolute top-0 cursor-pointer"
+                onClick={goBackHome}
+              >
+                &larr;
+              </span>
+            </div>
+            <span className="text-xl">{roomId}</span>
+            <div></div>
           </div>
           <ul
             className="relative mt-24 mb-6 flex h-full w-full flex-col gap-5 overflow-y-scroll px-5 scrollbar-hide "
@@ -104,13 +131,43 @@ const Home: NextPage = () => {
             className="fixed inset-x-0 bottom-0 bg-[#F7F9FC] py-8"
             onSubmit={handleSubmit(onValid)}
           >
-            <div className="relative mx-auto flex w-full max-w-lg items-center">
-              <span></span>
+            <div className="relative mx-auto flex w-full max-w-md items-center sm:max-w-xl">
+              <div className="absolute left-3 flex h-8 w-8 items-center justify-center rounded-full bg-white shadow-lg hover:shadow-xl">
+                <span
+                  className="absolute -top-[2px] left-[8px] cursor-pointer text-2xl font-bold "
+                  onClick={toggleShowMediaBox}
+                >
+                  +
+                </span>
+                <div
+                  className={cls(
+                    "-top-24 left-0 w-28 rounded-md bg-white py-2 px-2 shadow-lg",
+                    showMediaBox ? "absolute" : "hidden"
+                  )}
+                >
+                  <div className="py-1">
+                    <input
+                      type="file"
+                      id="file_upload"
+                      className="hidden"
+                      onChange={handleChangeFile}
+                    />
+                    <label htmlFor="file_upload" className="text-sm">
+                      파일 업로드
+                    </label>
+                  </div>
+                  <div className="border-t py-1">
+                    <span className="text-sm">이모지</span>
+                  </div>
+                </div>
+              </div>
               <input
                 type="text"
-                className="h-12 w-full rounded-full bg-[#EDF2FC] bg-opacity-70 pl-6 placeholder-[#1F1F1F] focus:border-gray-400 focus:bg-white focus:text-black focus:shadow-xl focus:outline-none"
+                className="h-12 w-full cursor-pointer break-words rounded-full bg-[#EDF2FC] bg-opacity-70 pl-14 pr-5 placeholder-[#1F1F1F] focus:border-gray-400 focus:bg-white focus:text-black focus:shadow-xl focus:outline-none"
                 placeholder={`#${roomId}에 메세지 보내기`}
                 {...register("message")}
+                autoFocus
+                autoComplete="off"
               />
             </div>
           </form>
